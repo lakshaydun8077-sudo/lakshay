1a0ff8ccf4a9afe5cf30d86b3bac2f39d415d2e6
lakshaydun8077-sudo <lakshaydun8077@gmail.com>
2026-08-20 10:31:52 +0530

    Add files via upload

---
+++ dayfirstquestionfirst.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+int a,b;
+printf ("ENTER a VALUE : ");
+scanf("%d" , &a);
+printf ("ENTER b VALUE : ");
+scanf("%d" , &b);
+int sum = a+b;
+printf("SUM IS : %d", sum);
+return 0;
+}
+
---
+++ dayfirstquestiontwo.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main(){
+float a,b;
+printf("Enter two numbers: ");
+scanf("%f %f", &a, &b);
+printf("sum = %f\n", a+b);
+printf("difference = %f\n", a-b);
+printf("product = %f\n", a*b);
+printf("quotient = %f\n", a/b);
+return 0;
+
+}
